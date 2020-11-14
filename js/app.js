ad63718d3602a8e319e70696e1aa7752522ca953
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2020-11-14 11:06:21 -0800

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,30 @@
 const toggleButton=document.getElementsByClassName('toggle-button')[0]
 const navLinks= document.getElementsByClassName('navLink')[0]
+const links = document.querySelectorAll('.navbarLink');
 
 toggleButton.addEventListener('click',()=> {
     navLinks.classList.toggle('active')
 })
+
+
+//active navigation on scroll
+for (const link of links) {
+    link.addEventListener("click", clickHandler);
+  }
+  
+  function clickHandler(e) {
+    e.preventDefault();
+    const href = this.getAttribute("href");
+    const offsetTop = document.querySelector(href).offsetTop;
+  
+    scroll({
+      top: offsetTop,
+      behavior: "smooth"
+    });
+  }
+  //
+  window.addEventListener('scroll',function(){
+      let nav = document.querySelector('.navbar');
+    let windoPosition = window.scrollY > 0
+      nav.classList.toggle('scrolling.active',windoPosition);
+  })
